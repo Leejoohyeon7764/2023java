37125bb1807dd11250110d0f9ac2ed42d4e18031
Leejoohyeon7764 <oo2596@naver.com>
2023-11-09 16:07:51 +0900

    ch8 done

---
+++ ch8/src/hos/Pet.java
@@ -0,0 +1,15 @@
+package hos;
+
+public interface Pet {
+   //protected String owner;
+   
+   
+         //public Pet(String owner)
+         //{
+         //   this.owner = owner;
+         //}
+         
+         String OwnerName();
+         void Cry();
+
+}
